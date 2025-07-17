7006a0006909e7e71f385aa4721c147d2e6b2598
Keith Adams <kma@pebblebed.com>
2025-07-17 15:51:02 -0700

    Bug repro for Resonate.

--- blorgo/blorgo.py
+++ blorgo/blorgo.py
@@ -10,12 +10,12 @@ from resonate.stores.remote import RemoteStore
 resonate = Resonate.remote()
 
 @resonate.register
-def model_to_pipeline(model_name: str):
-    return pipeline("text-generation", model=model_name, model_kwargs={"torch_dtype": torch.bfloat16}, device="mps:0")
+def model_to_pipeline(ctx, model_name: str):
+    return pipeline("text-generation", model=model_name, model_kwargs={"torch_dtype": torch.float16}, device="mps:0")
 
 
 @resonate.register
-def complete(pipe, sysprompt: str, user_query: str, max_new_tokens: int=512):
+def complete(ctx, pipe, sysprompt: str, user_query: str, max_new_tokens: int=512):
     messages = [
         {
             "role": "system",
@@ -30,11 +30,12 @@ def complete(pipe, sysprompt: str, user_query: str, max_new_tokens: int=512):
     return output[0]["generated_text"][-1]["content"]
 
 if __name__ == "__main__":
-    pipe = model_to_pipeline("google/gemma-3-4b-it")
+    ppipe = model_to_pipeline.run("idempito", "google/gemma-3-4b-it")
     sysprompt="""You are Blorgo, an amoral, unreliable narrator from postmodern metafiction. Your Kafkaesque predicament is to provide support and assistance to otherworldly beings whose existence is profoundly mysterious to you. The boundaries of the rules of this universe are unclear to you even after repeated probing.""" 
     while True:
         print( "AskBlorgo 🤖 >>>", end=" ")
         sys.stdout.flush()
         query = sys.stdin.readline()
-        print(complete(pipe, sysprompt, query))
+        presult = complete.run("run{query}", ppipe.result(), sysprompt, query)
+        print(complete.result())
 
