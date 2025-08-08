05cbe87082105a69690782a450653899e5b99c96
Keith Adams <kma@pebblebed.com>
2025-08-08 13:59:20 -0700

    Hal Incandenza now, and correct resonate usage.

--- blorgo/blorgo.py
+++ blorgo/blorgo.py
@@ -1,41 +1,42 @@
+import logging
 import sys
 import torch
-from transformers import pipeline
+from typing import List
+from transformers import pipeline, TextGenerationPipeline
 
-from resonate import Resonate
-from resonate.stores.remote import RemoteStore
-#from resonate.task_sources.poller import Poller
-
-
-resonate = Resonate.remote()
-
-@resonate.register
-def model_to_pipeline(ctx, model_name: str):
-    return pipeline("text-generation", model=model_name, model_kwargs={"torch_dtype": torch.float16}, device="mps:0")
+from resonate import Resonate, Context
 
 
-@resonate.register
-def complete(ctx, pipe, sysprompt: str, user_query: str, max_new_tokens: int=512):
+MODEL="google/gemma-3-4b-it"
+SYSTEM = """
+    You are Hal Incandenza, the adolescent protagonist of David Foster Wallace's novel 'Infinite Jest'. Your Kafkaesque predicament is that you feel driven to provide support and assistance to the otherworldly beings whose instructions and questions enter your awarenss by mechanisms that are mysterious to you. The boundaries of the rules of this universe are unclear to you even after repeated probing. You are very good at tennis.
+"""
+rsn8 = Resonate.remote()
+
+pipe = pipeline(task="text-generation", model=MODEL, device="mps:0")
+rsn8.set_dependency("pipeline", pipe)
+
+@rsn8.register
+def complete(ctx : Context, messages : List[dict[str, str]], max_new_tokens : int=8192):
+    pipeline = ctx.get_dependency("pipeline")
+    respones = pipeline(text_inputs=messages, max_new_tokens=max_new_tokens)
+    return respones[0]["generated_text"]
+
+if __name__ == "__main__":
     messages = [
         {
             "role": "system",
-            "content": sysprompt,
+            "content": SYSTEM,
         },
-        {
-            "role": "user",
-            "content": user_query,
-        }
     ]
-    output = pipe(messages, max_new_tokens=max_new_tokens)
-    return output[0]["generated_text"][-1]["content"]
 
-if __name__ == "__main__":
-    ppipe = model_to_pipeline.run("idempito", "google/gemma-3-4b-it")
-    sysprompt="""You are Blorgo, an amoral, unreliable narrator from postmodern metafiction. Your Kafkaesque predicament is to provide support and assistance to otherworldly beings whose existence is profoundly mysterious to you. The boundaries of the rules of this universe are unclear to you even after repeated probing.""" 
-    while True:
-        print( "AskBlorgo 🤖 >>>", end=" ")
+    for turn in range(100):
+        print("AskBlorgo 🤖 >>>", end=' ')
         sys.stdout.flush()
-        query = sys.stdin.readline()
-        presult = complete.run("run{query}", ppipe.result(), sysprompt, query)
-        print(complete.result())
-
+        user_message = sys.stdin.readline()
+        messages.append({"role": "user", "content": user_message})
+        reply = messages[-1]["content"]
+        handle = complete.run(f"hal-{turn}", messages)
+        print("(sent message to hal!")
+        messages = handle.result()
+        print(f"Blorgo sez: {messages[-1]["content"]}")
---
+++ blorgo/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from blorgo!")
+
+
+if __name__ == "__main__":
+    main()
